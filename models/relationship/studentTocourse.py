739422ecd34588eb8b04383196a8835684fe9268
Mohamed Emad <mohamed.emad4bubble@gmail.com>
2024-12-21 11:35:33 +0200

    sql alchamy joins

--- models/relationship/studentTocourse.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
-
-class StudentCourse(db.Model):
-    course_id: Mapped[int] = mapped_column(primary_key=True)
-    student_id: Mapped[str] = mapped_column(unique=True)
-    
